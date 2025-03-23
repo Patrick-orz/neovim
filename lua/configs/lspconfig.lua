34231dfc2cbcfcac7260741fb021e05919516abd
Patrick <patrick_tang@outlook.com>
2025-03-23 17:32:47 -0400

    feat(lsp): switch back to pylyzer

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- servers
-local servers = { "html", "cssls" , "gdscript" , "clangd", "pylsp"}
+local servers = { "html", "cssls" , "gdscript" , "clangd", "pylyzer"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
